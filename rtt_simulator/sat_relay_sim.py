685827fd5eaea41d25e293c19871ddadbda70cfb
Aleksandr Stevens <aleksandrstevens@Aleksandrs-MBP.lan>
2021-12-23 20:11:42 -0800

    Wrote unit tests for satellite portion of rtt simulator

--- rtt_simulator/sat_relay_sim.py
+++ rtt_simulator/sat_relay_sim.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("../satgenpy")
+sys.path.append(os.path.abspath("../satgenpy"))
 
 import satgen
 import os
@@ -10,11 +10,13 @@ import tempfile
 import sim_config
 import constants
 from collections import defaultdict
-from satgen.post_analysis.analyze_rtt import print_routes_and_rtt
+from satgen.post_analysis.print_routes_and_rtt import print_routes_and_rtt
 
 def SatelliteNetworkState():
-    def __init__(self, constellation_config, duration, output_dir):
+    #gs_points is a generator of NetworkPoints
+    def __init__(self, constellation_config, gs_points, duration, output_dir):
         self.constellation = constellation_config
+        self.groundstation_points = gs_points
         self.output_dir = output_dir
         self.duration = duration
         self.loc_to_id = defaultdict(lambda: -1)
@@ -26,7 +28,7 @@ def SatelliteNetworkState():
                     quoting=csv.QUOTE_MINIMAL)
 
             id = 0
-            for net_point in self.config.network_points():
+            for net_point in self.groundstation_points:
                 if net_point.type() == constants.GS_POINT_TYPE:
                     gs_name = net_point.name()
                     row = [id, gs_name] + net_point.location() + [0.0]
@@ -91,23 +93,87 @@ def SatelliteNetworkState():
             True
         )
 
-    def get_groundstation_id(self, name):
-        if name not in self.loc_to_id:
-            raise ValueError("No ID associated with '" + name + "'")
-        return self.loc_to_id[name]
-
-
-class RelaySimulator():
-    def __init__(self, config):
-        self.config = config
-        self.state_dir = tempfile.mkdtemp()
-        self.sim_state = SatelliteNetworkState(self.config.constellation(), self.config.duration, self.state_dir)
-        self.sim_state.create()
-
-    def run(self, src_name, dst_name):
-        src_id = self.sim_state.get_groundstation_id(src_name)
-        dst_id = self.sim_state.get_groundstation_id(dst_name)
-        print_routes_and_rtt(self.config.output_dir, state_dir, constants.TIMESTEP_MS, self.config.duration, src_id, dst_id, "")
+    def groundstation_map(self, save_to_fname=None):
+        return GroundstationMap(save_to_fname, gs_map=self.loc_to_id)
 
     def cleanup(self):
-        shutil.rmtree(self.state_dir)
+        shutil.rmtree(self.output_dir)
+
+class GrounstationMap():
+    def __init__(self, gs_fname, gs_map=None):
+        self.name_to_id_map = gs_map
+        self.fname = gs_fname
+
+    def load(self):
+        if not os.path.exists(self.fname):
+            raise ValueError("No such file '" + self.fname + "' to load from")
+        if not os.access(self.fname, os.R_OK):
+            raise ValueError("Access Denied to read file '" + self.fname + "'")
+
+        self.name_to_id_map = {}
+        try:
+            with open(self.fname, "r") as fp:
+                reader = csv.reader(fp)
+                for row in reader:
+                    self.name_to_id_map[int(row[0])] = row[1]
+        except:
+            raise IOError("Unexpected Error reading '" + self.fname + "': " + str(self.exc_info()[0]))
+
+    def save(self):
+        if self.fname is None:
+            raise ValueError("No write filename was specified")
+
+        try:
+            with open(self.fname, "w") as fp:
+                writer = csv.writer(fp, delimiter=",", quotechar="\"", quoting=csv.QUOTE_MINIMAL)
+                for id, gs_name in self.name_to_id_map.items():
+                    writer.write_row([id, gs_name])
+        except:
+            raise IOError("Unexpected Error reading '" + self.fname + "': " + str(self.exc_info()[0]))
+
+    def get_groundstation_id(self, name):
+        if self.name_to_id_map is None:
+            raise ValueError("No groundstation name to ID map has been loaded")
+        if name not in self.name_to_id_map:
+            raise ValueError("No ID associated with '" + name + "'")
+        return self.name_to_id_map[name]
+
+
+class SatelliteRelaySimulator():
+    def __init__(self, gs_map, duration, state_dir, output_dir):
+        self.state_dir = state_dir
+        self.output_dir = output_dir
+        self.duration = duration
+        self.rtt_datapoints = None
+        self.gs_name_to_id_map = gs_map
+
+    def run(self, src_name, dst_name):
+        src_id = self.gs_name_to_id_map.get_groundstation_id(src_name)
+        dst_id = self.gs_name_to_id_map.get_groundstation_id(dst_name)
+        print_routes_and_rtt(self.output_dir, self.state_dir, constants.TIMESTEP_MS, self.duration, src_id, dst_id, "")
+
+        rtt_filename = self.output_dir + "/" + str(constants.TIMESTEP_MS) + "ms_for_" + str(self.duration) + "s/" + \
+            "manual/data/networkx_rtt_" + str(src_id) + "_to_" + str(dst_id)
+
+        with open(rtt_filename, "r") as rtt_file:
+            reader = csv.reader(rtt_file)
+            self.rtt_datapoints = []
+            for row in reader:
+                rtt_in_seconds = float(row[1]) / 1000000000.0
+                self.rtt_datapoints.append(rtt_in_seconds)
+
+    def all_rtts(self):
+        for rtt_in_s in self.rtt_datapoints:
+            yield rtt_in_s
+
+    def avg_rtt(self):
+        avg = 0.0
+        for rtt_in_s in self.all_rtts():
+            avg += rtt_in_s
+            total += 1
+
+        avg /= float(total)
+        return avg
+
+    def cleanup(self):
+        shutil.rmtree(self.output_dir)
---
+++ test/test_rtt_simulator.py
@@ -0,0 +1,67 @@
+import sys
+import os
+import tempfile
+sys.path.append(os.path.abspath("./rtt_simulator"))
+
+import sat_relay_sim
+import constants
+import constellation_config
+
+def test_sat_sim():
+    state_dir = os.path.abspath("./starlink_test_state_100ms_for_200s")
+
+    gmap = sat_relay_sim.GroundstationMap(state_dir + "/" + id_map)
+    gmap.load()
+
+
+    with tempfile.TemporaryDirectory() as tempdir_name:
+        duration = 200 #seconds
+        src_name = "start"
+        dst_name = "end"
+
+        simulator = sat_relay_sim.SatelliteRelaySimulator(gmap, duration, state_dir, tempdir_name)
+        simulator.run(src_name, dst_name)
+
+        src_id = gmap.get_groundstation_id(src_name)
+        dst_id = gmap.get_groundstation_id(dst_name)
+        rtt_fname = tempdir_name + "/" + str(constants.TIMESTEP_MS) + "ms_for_" + str(duration) + "s/" + \
+            "manual/data/networkx_rtt_" + str(src_id) + "_to_" + str(dst_id)
+
+        #Make sure simulator.run created the output file
+        assert os.file.exists(rtt_fname)
+
+        total = 0
+        avg = 0.0
+        for rtt in simulator.all_rtts():
+            avg += rtt
+            total += 1
+        avg /= total
+
+        #Make sure simulator generated all 2000 datapoints
+        assert total == 2000
+
+        #Make sure average rtt calculation is correct
+        assert simulator.avg_rtt() == avg
+
+def test_sat_state_gen():
+    const_config = constellation_config.GetStarlinkConfig()
+    netpoints = (net_point for net_point in [ \
+        NetworkPoint("Tokyo", constants.GS_POINT_TYPE, "Tokyo", lat=35.6895, long=139.69171), \
+        NetworkPoint("Shanghai", constants.GS_POINT_TYPE, "Shanghai", lat=31.22222, long=121.45806 \
+        ])
+
+    with tempfile.TemporaryDirectory() as tempdir_name:
+        net_state = sat_relay_sim.SatelliteNetworkState(const_config, netpoints, 1, tempdir_name)
+        net_state.create()
+
+        assert os.file.exists(tempdir_name + "/ground_stations.txt")
+        assert os.file.exists(tempdir_name + "/isls.txt")
+        assert os.file.exists(tempdir_name + "/description.txt")
+        assert os.file.exists(tempdir_name + "/gsl_interfaces_info.txt")
+
+        gmap = net_state.groundstation_map()
+
+        with tempfile.TemporaryDirectory() as sim_tempdir_name:
+            sat_sim = sat_relay_sim.SatelliteRelaySimulator(gmap, 1, tempdir_name, sim_tempdir_name)
+            sat_sim.run()
+            assert sat_sim.avg_rtt() > 0
